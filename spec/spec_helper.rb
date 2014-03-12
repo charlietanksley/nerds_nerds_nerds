1dcd8aa5e6e2404a2661c4b2bf37bbc24a563573
Charlie Tanksley <charlie.tanksley@gmail.com>
2014-03-12 10:04:51 -0400

    if I knew how to write code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 require 'nerds_nerds_nerds/html'
 module SpecMacros
-  def mock_about_us_page
+  def self.mock_about_us_page
     File.open('spec/support/files/about_us.html') { |f| f.read }
   end
 
-  def nerd_html
+  def self.nerd_html
     NerdsNerdsNerds::HTML.new(
       content: self.mock_about_us_page)
+      .parse
   end
 end
