da96852b68102f3f6634571464efdb63773d0f7d
Steven Harman <steven@harmanly.com>
2014-03-13 15:25:30 -0400

    Prefer stubs for queries
    Reserving mocks for commands. See:
    http://stevenharman.net/sensible-testing-is-about-sensible-design "To
    Stub or To Mock"

--- spec/nerds_nerds_nerds/nerd_spec.rb
+++ spec/nerds_nerds_nerds/nerd_spec.rb
@@ -6,8 +6,7 @@ describe NerdsNerdsNerds::Nerd do
     data = double('Nokogiri::HTML::Document')
     css_node = double('node', inner_html: 'me first')
 
-    expect(data).to receive(:at_css)
-      .and_return(css_node)
+    allow(data).to receive(:at_css) { css_node }
     nerd = NerdsNerdsNerds::Nerd.new(data)
     expect(nerd.html_id).to eq('me-first')
   end
@@ -17,8 +16,7 @@ describe NerdsNerdsNerds::Nerd do
     css_node = double('node', attribute_nodes:
       Array[double('text', inner_text: 'image.jpg')])
 
-    expect(data).to receive(:at_css)
-      .and_return(css_node)
+    allow(data).to receive(:at_css) { css_node }
     nerd = NerdsNerdsNerds::Nerd.new(data)
     expect(nerd.image(basepath: 'http://foo')).to eq('http://foo/image.jpg')
   end
